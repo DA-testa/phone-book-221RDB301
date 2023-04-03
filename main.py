7b8d094a7daeb875eb408b80d7cdbd56c3826b8a
221RDB301 <118080877+221RDB301@users.noreply.github.com>
2023-04-03 14:03:59 +0000

    optimized

--- main.py
+++ main.py
@@ -17,29 +17,27 @@ def write_responses(result):
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add i
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
-        else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+    contacts = {}
+    
+    def process_add(query):
+        contacts[query.number] = query.name
+    
+    def process_del(query):
+        if query.number in contacts:
+            del contacts[query.number]
+    
+    def process_find(query):
+        result.append(contacts.get(query.number, 'not found'))
+    
+    process_query = {
+        'add': process_add,
+        'del': process_del,
+        'find': process_find
+    }
+    
+    for query in queries:
+        process_query[query.type](query)
+    
     return result
 
 if __name__ == '__main__':
